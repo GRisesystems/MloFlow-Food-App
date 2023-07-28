ce89d9e7642692ab84717f37b170aa88231697bf
isaac <isaac@localhost.localdomain>
2023-07-28 12:25:57 +0300

    top nav image place holder

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,10 +1,13 @@
 // TopNav, NavBar and Footer sections
 import { Outlet } from 'react-router-dom'
 import TopNav from './includes/TopNav'
+import NavBar from './includes/NavBar'
+
 const Root = () => {
   return (
     <>
         <TopNav/>
+        <NavBar/>
             <Outlet/>
         {/* footer section */}
     </>
