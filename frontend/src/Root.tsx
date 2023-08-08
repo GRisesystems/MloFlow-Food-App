35a767a2ac47c656b8197546420f0688b00875a3
isaac <isaac@localhost.localdomain>
2023-08-08 16:34:27 +0300

    merge1

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,21 +1,17 @@
 // TopNav, NavBar and Footer sections
-// import { Outlet } from 'react-router-dom'
-// import TopNav from './includes/TopNav'
-// import NavBar from './includes/NavBar'
-// import { FooterContainer } from './includes/Footer'
-import Form from './components/homeScreen/SignUp'
-
-
+import { Outlet } from 'react-router-dom'
+import TopNav from './includes/TopNav'
+import NavBar from './includes/NavBar'
+import { FooterContainer } from './includes/Footer'
 
 const Root = () => {
   return (
     <>
-        {/* <TopNav/>
-        <NavBar/> */}
-            {/* <Outlet/> */}
+        <TopNav/>
+        <NavBar/>
+            <Outlet/>
             <div style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}>
-              <Form />
-        {/* <FooterContainer /> */}
+        <FooterContainer />
       </div>
          
       
