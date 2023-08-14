40bcc5862337cdb356b074fd3f8ee350fac6b980
isaac <isaac@localhost.localdomain>
2023-08-14 09:22:29 +0300

    chefs dashboard

---
+++ frontend/src/screens/chefDashboardScreen/ChefDashBoardScreen.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+const ChefDashBoardScreen = () => {
+  return (
+    <Box sx={{minHeight:'80vh'}}>ChefDashBoardScreen</Box>
+  )
+}
+
+export default ChefDashBoardScreen
