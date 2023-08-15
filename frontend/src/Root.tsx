8310089dd1d624e6fd58d4896e677c321c830ccd
isaac <isaac@localhost.localdomain>
2023-08-15 11:31:29 +0300

    auth provider wrapper in app root section

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -3,15 +3,16 @@ import { Outlet } from 'react-router-dom'
 import TopNav from './includes/TopNav'
 import NavBar from './includes/NavBar'
 import { FooterContainer } from './includes/Footer'
+import {AuthProvider} from './utils/AuthContext'
 
 const Root = () => {
   return (
-    <>
+    <AuthProvider>
         <TopNav/>        
         <NavBar/>
             <Outlet/>
         <FooterContainer />       
-    </>
+    </AuthProvider>
   )
 }
 
