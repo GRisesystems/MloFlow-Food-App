5ac0fe4e1953952496bbd5cca2d1916487885451
FaithSang <faithsang001@gmail.com>
2023-08-17 13:32:04 +0300

    resolved conflicts

--- backend/Mloflo/authapp/models.py
+++ backend/Mloflo/authapp/models.py
@@ -72,12 +72,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-<<<<<<< HEAD
-    is_first_time_login = models.BooleanField(default=True)
-=======
     if_first_time_login = models.BooleanField(default=True)
     #Terms_and_condition = models.BooleanField(default=True)
->>>>>>> origin/ft_kamakia3
 
 
 
