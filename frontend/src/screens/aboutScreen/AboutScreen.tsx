8053ee40e77a6408bb6eeceb013b9e7211334767
isaac <isaac@localhost.localdomain>
2023-08-17 14:22:58 +0300

    about page card design update

--- frontend/src/screens/aboutScreen/AboutScreen.tsx
+++ frontend/src/screens/aboutScreen/AboutScreen.tsx
@@ -1,17 +1,25 @@
 import TopSection from "../../components/aboutScreen/TopSection"
 import ServicesCard from "../../components/aboutScreen/ServicesCard"
-import { Box, Container } from "@mui/material"
+import { Box, Container, CssBaseline, Grid, Typography } from "@mui/material"
 
 const AboutScreen = () => {
   return (
     <Box>
-        <TopSection/>
-        <Container sx={{boxShadow: 3,mt:3}}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat ipsa dolor beatae! Debitis aut optio quae incidunt voluptatibus eum, nihil exercitationem aperiam possimus nesciunt, placeat voluptatem omnis, doloribus pariatur fugiat?</Container>
-        <Box>
-            <ServicesCard/>
-            <ServicesCard/>
-            <ServicesCard/>
+      <CssBaseline/>
+      <TopSection />
+      <Container sx={{mt:3}}>
+        <Box sx={{textAlign:'center'}}>
+          <Typography variant="h6" >About</Typography>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora aperiam reprehenderit nam, numquam ad dolor accusamus culpa. Rem nihil nam ab, laborum commodi, at soluta iusto, ipsum voluptatibus culpa aut.
+          </Typography>
         </Box>
+        <Grid container spacing={7}>
+          <ServicesCard/>
+          <ServicesCard/>
+          <ServicesCard/>
+        </Grid>      
+      </Container>
     </Box>
   )
 }
