2808b3e3a01a57af230a001cd69f7064f6e88ed4
FaithSang <faithsang001@gmail.com>
2023-08-22 13:11:12 +0300

    Modidied the access token to only have the user id  then use the id to get firstname, surname, if_first_time_login and category.

--- backend/Mloflo/authapp/views.py
+++ backend/Mloflo/authapp/views.py
@@ -1,5 +1,7 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .models import User
+import uuid
 from rest_framework.response import Response
 import jwt
 
@@ -8,13 +10,10 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def get_token(cls, user):
         token = super().get_token(user)
 
-        # Add custom claims       
-        token['first_name'] = user.first_name
-        token['surname'] = user.surname
-        token['first_time_login'] = user.if_first_time_login
-        token['category'] = user.category
-        return token
+         # Add custom claims       
+        token['user_id'] = str(user.id)
 
+        return token
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -24,16 +23,19 @@ class MyTokenObtainPairView(TokenObtainPairView):
         
         if response.status_code == 200:
             access_token = response.data['access']
-            payload = jwt.decode(access_token, options={"verify_signature": False})  # Decode the access token payload
+            payload = jwt.decode(access_token, options={"verify_signature": False})
+            
+            user_id_str = payload.get('user_id')
+            user_id = uuid.UUID(user_id_str) # Convert the string back to a UUID
+            
+            # Query your user management system to get additional user data based on user_id
+            user = User.objects.get(id=user_id)
             
             data = response.data
-            data['first_name'] = payload.get('first_name')
-            data['surname'] = payload.get('surname')
-            data['first_time_login'] = payload.get('first_time_login')
-            data['category'] = payload.get('category')
+            data['first_name'] = user.first_name
+            data['surname'] = user.surname
+            data['first_time_login'] = user.if_first_time_login
+            data['category'] = user.category
             
         return response
-    def _get_payload(self, token):
-        from rest_framework_simplejwt.tokens import RefreshToken
-        refresh_token = RefreshToken(token)
-        return refresh_token.payload
+
