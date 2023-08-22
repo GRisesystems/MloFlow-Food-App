0e04f13931f15047296021b6bb7ccdaa00cc922c
isaac <isaac@localhost.localdomain>
2023-08-22 14:30:25 +0300

    payments app url configuration

---
+++ backend/Mloflo/payments/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('mpesa', views.mpesa_payment, name='mpesa_payment'),
+    path('mpesa', views.initiate_stk_push, name='mpesa_payment'),
+]
