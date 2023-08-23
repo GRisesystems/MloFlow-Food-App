29a7b954c166b474356d907c11138a1271cecadc
isaac <isaac@localhost.localdomain>
2023-08-23 10:03:22 +0300

    login form redirect

--- backend/Mloflo/Mloflo/settings.py
+++ backend/Mloflo/Mloflo/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-uc&_xy5q3skrh9yb^@(+k@6c03rkit6hrwt6=@$omy#*u!=b=$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 LOGIN_URL = '/login/'
