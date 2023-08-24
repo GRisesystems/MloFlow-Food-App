69d36f0b60f5967f0c44170709f00aefc63ee72a
isaac <isaac@localhost.localdomain>
2023-08-24 11:54:11 +0300

    function to protect routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Navigate, Route, RouterProvider } from 'react-router-dom'
 import HomeScreen from "./screens/homeScreen/HomeScreen"
 import SignInScreen from './screens/SignInScreen/SignInScreen'
 import ChefDashBoardScreen from './screens/chefDashboardScreen/ChefDashBoardScreen'
@@ -14,15 +14,30 @@ import FishProductsScreen from './screens/fishProductsScreen/FishProductsScreen'
 import CookedProductsScreen from './screens/cookedProductsScreen/CookedProductsScreen'
 import PoultryProductsScreen from './screens/poultryProductsScreen/PoultryProductsScreen'
 
+import { useAuth } from './utils/AuthContext';
+
+
+
+const ProtectedRoute = ({ element: Element, ...rest }: { element: React.ElementType }) => {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    localStorage.setItem('redirectAfterLogin', window.location.pathname);
+    return <Navigate to="/login" />;
+  }
+
+  return <Element {...rest} />;
+};
+
+
 
 const router = createBrowserRouter(
-  // App router configuration. 
   createRoutesFromElements(
     <Route path='/' element={<Root />}>
       <Route index element={<HomeScreen />} />
       <Route path='/login' element={<SignInScreen />} />
       <Route path='/chef-dashboard' element={<ChefDashBoardScreen />} />
-      <Route path='/vendor-dashboard' element={<VendorDashboardScreen />} />
+      <Route path="/vendor-dashboard" element={<ProtectedRoute element={VendorDashboardScreen} />} />
       <Route path='/customer-dashboard' element={<CustomerDashboardScreen />} />
       <Route path='/farm-produce' element={<FarmProduceScreen />} />
       <Route path='/fish-products' element={<FishProductsScreen />} />
@@ -31,7 +46,7 @@ const router = createBrowserRouter(
 
       <Route path='/cart' element={<Cart onClose={function (): void {
         throw new Error('Function not implemented.')
-      } } />} />
+      }} />} />
       <Route path='/checkout' element={<CheckoutScreen />} />
       <Route path='/about' element={<AboutScreen />} />
 
