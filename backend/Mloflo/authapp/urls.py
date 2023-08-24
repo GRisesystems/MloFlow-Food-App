a3bcf3d9ea7684e9ad1f5de22c8ea515cff36c86
engkamakia <georgekamakia@gmail.com>
2023-08-24 10:32:50 +0300

    feature to check if is first time login

--- backend/Mloflo/authapp/urls.py
+++ backend/Mloflo/authapp/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 
 
 from .views import *
-from .views import MyTokenObtainPairView
+#from .views import MyTokenObtainPairView
 
-from rest_framework_simplejwt.views import (
-    TokenRefreshView,
-)
+#from rest_framework_simplejwt.views import (
+  #  TokenRefreshView,
+#)
+
+from . import views
 
 urlpatterns = [  
     
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    #path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("login/", views.LoginView.as_view(), name="login"),
 ]
