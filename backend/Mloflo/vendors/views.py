e3479d0662c42e6b03d67fa43213d295fac0c922
isaac <isaac@localhost.localdomain>
2023-08-26 13:23:41 +0300

    update vendor details view

--- backend/Mloflo/vendors/views.py
+++ backend/Mloflo/vendors/views.py
@@ -1,13 +1,39 @@
-
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
+from Category.models import Category
+from .models import Vendor
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def update_vendor_details(request):
-    context = {}
-    context['message'] = 'success'
-    return Response(context, status=status.HTTP_201_CREATED)
+    try:
+        data = request.data
+        country = data.get('country')
+        state = data.get('state')
+        city = data.get('city')
+        selected_options = data.get('selectedOptions')
+
+        # Retrieve  the vendor's product categories
+        product_categories = [Category.objects.get(
+            id=category_id) for category_id in selected_options]
+
+        # create vendor data
+        vendor = Vendor.objects.create(
+            vendor=request.user,
+            country=country,
+            state=state,
+            city=city,
+        )
+        # set product categories
+        vendor.product_category.set(product_categories)
+
+        context = {'message': 'Vendor details updated successfully'}
+        return Response(context, status=status.HTTP_201_CREATED)
+
+    except Exception as e:
+        context = {'error': str(e)}
+        print(e)
+        return Response(context, status=status.HTTP_400_BAD_REQUEST)
