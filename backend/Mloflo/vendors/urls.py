247db186d20533c21e38f179d5579ec7326f2455
isaac <isaac@localhost.localdomain>
2023-08-26 11:24:44 +0300

    update vendor details view

--- backend/Mloflo/vendors/urls.py
+++ backend/Mloflo/vendors/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.update_vendor_details, name='update_vendor_details'),
 ]
--- backend/Mloflo/vendors/views.py
+++ backend/Mloflo/vendors/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def update_vendor_details(request):
+    context = {}
+    context['message'] = 'success'
+    return Response(context, status=status.HTTP_201_CREATED)
