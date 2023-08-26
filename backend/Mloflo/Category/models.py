4fea27873ce1ac29b042a23fe6ec8739db7720af
isaac <isaac@localhost.localdomain>
2023-08-26 13:24:03 +0300

    vendor details model

--- backend/Mloflo/Category/models.py
+++ backend/Mloflo/Category/models.py
@@ -1,7 +1,12 @@
+import uuid
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True,
+                          editable=False, default=uuid.uuid4)
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
