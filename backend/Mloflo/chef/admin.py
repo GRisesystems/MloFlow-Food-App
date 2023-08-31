1c9390713aa47335e1f738448463c6e2388e6ca0
KEVIN KIPKEMOI <kevin@pezesha.com>
2023-08-31 21:33:00 +0300

    modified:  backend/Mloflo/chef/admin.py @author: <NAME> <<EMAIL>>

--- backend/Mloflo/chef/admin.py
+++ backend/Mloflo/chef/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from . models import ChefTestTable
+from .models import Chef, Review
+
 
 # Register your models here.
 
-admin.site.register(ChefTestTable)
+admin.site.register(Chef)
+admin.site.register(Review)
