8c8278a8c167d38c4dc47f9ba47a1c0a2ea29c10
KEVIN KIPKEMOI <kevin@pezesha.com>
2023-08-31 21:34:00 +0300

    modified:  backend/Mloflo/chef/urls.py

--- backend/Mloflo/chef/urls.py
+++ backend/Mloflo/chef/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ChefListCreateView, ChefRetrieveUpdateDestroyView
+from .views import ChefListCreateView, ChefRetrieveUpdateDestroyView,ReviewListCreateView,ReviewRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('chefs/', ChefListCreateView.as_view(), name='chef-list-create'),
     path('chefs/<int:pk>/', ChefRetrieveUpdateDestroyView.as_view(), name='chef-detail'),
+    path('reviews/', ReviewListCreateView.as_view(), name='review-list-create'),
+    path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='review-detail'),
 ]
+
