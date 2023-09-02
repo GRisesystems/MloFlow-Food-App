8aa3d7bea16bee6fa2a11d06bcf804a01b83fcdb
FaithSang <faithsang001@gmail.com>
2023-09-02 19:15:24 +0300

    Modified the images field on the products app

--- backend/Mloflo/products/serializers.py
+++ backend/Mloflo/products/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
-from .models import Product, ProductImage
+from .models import Product
 from authapp.models import User
 
 
-class ProductImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ProductImage
-        fields = ('id', 'image',)
+# class ProductImageSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = ProductImage
+#         fields = ('id', 'image',)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -14,13 +14,13 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'email')  # Customize the fields you want to expose
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
-     created_by = UserSerializer(read_only=True)
-     images = ProductImageSerializer(many=True, read_only=True)
+    #  created_by = UserSerializer(read_only=True)
+    #  images = ProductImageSerializer(many=True, read_only=True)
 
      class Meta:
         model = Product
-        fields = ( "name", "category", "description", "price", "stock", "weight", "images")
-        read_only_fields = ['created_by']
+        fields = ( "name", "category", "description", "price", "stock", "weight", "imageOne", "imageTwo", "imageThree", "imageFour")
+        # read_only_fields = ['created_by']
         fields ='__all__'
 
 
