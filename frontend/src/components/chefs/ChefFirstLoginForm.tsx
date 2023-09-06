e61822e44c8002b1b06965120cf2b46f35e76352
isaac <isaac@localhost.localdomain>
2023-09-06 11:00:31 +0300

    chef form input

--- frontend/src/components/chefs/ChefFirstLoginForm.tsx
+++ frontend/src/components/chefs/ChefFirstLoginForm.tsx
@@ -1,24 +1,59 @@
-import { Box, Button, CardMedia, Dialog, DialogContent, DialogContentText, DialogTitle, Divider, FormControl, InputLabel, MenuItem, Paper, Select, TextField, Typography } from "@mui/material"
-import { Controller, useForm } from "react-hook-form"
+import React, { useState } from 'react';
+import {
+    Box,
+    Button,
+    CardMedia,
+    Dialog,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Divider,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Paper,
+    Select,
+    TextField,
+    Typography,
+} from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
 import { Country, State } from 'country-state-city';
-import { useEffect, useState, useRef } from "react";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import { useEffect, useRef } from 'react';
+import { useAuth } from "../../utils/AuthContext";
+import { BASE_URL } from "../signin/constants";
+import FileUpload from './FileUpload'; // Adjust the path as needed
+import axios from 'axios';
 
-const ChefFirstLoginForm = () => {
-    const specialities = ['sea food', 'fried chicken', 'Mukimo', 'Pilau', 'Fufu']
-    const { control, register, handleSubmit, formState: { errors }, watch } = useForm()
-    const countries = Country.getAllCountries()
+
+const ChefFirstLoginForm = () => {    
+    const specialities = ['sea food', 'fried chicken', 'Mukimo', 'Pilau', 'Fufu'];
+    const { accessToken, updateProfileData } = useAuth();
+    const {
+        control,
+        register,
+        handleSubmit,
+        formState: { errors },
+        watch,
+        setValue,
+    } = useForm();
+    const allcountries = Country.getAllCountries();
+    const [countries, setCountries] = useState(allcountries);
+    const [states, setStates] = useState([]);
+    const [selectedCountryName, setSelectedCountryName] = useState('');
     const [selectedCountry, setSelectedCountry] = useState<string | null>(null); // Initialize with null
     const [selectedStates, setSelectedStates] = useState<State[]>([]);
     const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
     const [previewImage, setPreviewImage] = useState<string | undefined>(undefined);
     const inputRef = useRef<HTMLInputElement | null>(null);
+    const [showFileUpload, setShowFileUpload] = useState(false);
+    const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
+    const [additionalFileData, setAdditionalFileData] = useState([]);
 
     useEffect(() => {
         // Fetch and update the list of states whenever the selected country changes
         if (selectedCountry) {
-            const selectedCountryData = countries.find(country => country.name === selectedCountry);
+            const selectedCountryData = countries.find((country) => country.name === selectedCountry);
             if (selectedCountryData) {
                 const countryStates = State.getStatesOfCountry(selectedCountryData.isoCode);
                 setSelectedStates(countryStates);
@@ -26,94 +61,98 @@ const ChefFirstLoginForm = () => {
         }
     }, [selectedCountry, countries]);
 
-    const handleFileChange = () => {
-        if (inputRef.current) {
-            const file = inputRef.current.files?.[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = () => {
-                    setPreviewImage(reader.result as string);
-                };
-                reader.readAsDataURL(file);
-            }
+    const handleCountryChange = (event) => {
+        const selectedCountryName = event.target.value; // Get selected country name
+        const selectedCountry = countries.find(country => country.name === selectedCountryName);
+
+        if (selectedCountry) {
+            setValue('country', selectedCountryName); // Update the form value
+            const countryStates = State.getStatesOfCountry(selectedCountry.isoCode);
+            setStates(countryStates);
+        }
+    };
+
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setPreviewImage(reader.result as string);
+            };
+            reader.readAsDataURL(file);
+
+            // Set the selected image file in the state
+            setSelectedImage(file);
         }
     };
 
 
-    const onSubmit = async (data: any) => {
-        console.log(data)
-    }
+    const toggleFileUpload = () => {
+        setShowFileUpload(!showFileUpload);
+    };
+
+    const onSubmit = async (data) => {
+        const formData = { ...data, 'profile_picture': selectedImage, 'cerifications': additionalFileData };
+
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${accessToken}`, // Include the accessToken
+            },
+        };
+        console.log(formData)
+        // try {
+        //     const response = await axios.post(`${BASE_URL}/chef/chefs/`, formData, config);
+        //     if (response.status == 201 || response.status == 200) {
+        //         const updatedData = await updateProfileData()
+        //         console.log(updatedData)
+
+        //     }
+
+        // } catch (error) {
+        //     console.error('API Error:', error);
+
+        // }
+    };
+
     return (
-        <Dialog
-            open={true}
-            fullWidth={true}
-        >
+        <Dialog open={true} fullWidth={true}>
             <DialogTitle sx={{ color: 'black' }}>Chefs Profile Information</DialogTitle>
             <Divider />
 
             <Box>
                 <Box
-                    component="form" noValidate autoComplete="off"
-                    onSubmit={
-                        handleSubmit(onSubmit)
-                    }
+                    component="form"
+                    noValidate
+                    autoComplete="off"
+                    onSubmit={handleSubmit(onSubmit)}
                     sx={{ m: 2 }}
                 >
-                    <Box>
-                        <InputLabel id="demo-simple-select-label" required sx={{ color: 'black' }}>Select Country</InputLabel>
-                        <Controller
-                            name="country"
-                            control={control}
-                            rules={{ required: false }}
-                            render={({ field }) => (
-                                <>
-
-                                    <Select
-                                        {...field}
-                                        autoFocus
-                                        fullWidth
-                                        inputProps={{
-                                            name: 'country',
-                                            id: 'country',
-                                        }}
-                                        value={selectedCountry || ''}
-                                        onChange={event => setSelectedCountry(event.target.value)}
-                                    >
-                                        {countries &&
-                                            countries.map((country) => (
-                                                <MenuItem key={country.isoCode} value={country.name}>
-                                                    {country.name}
-                                                </MenuItem>
-                                            ))}
-                                    </Select>
-                                    {errors.country && <span>This field is required</span>}
-                                </>
-                            )}
-                        />
+                    <Box sx={{ mt: 2 }}>
+                        <InputLabel required sx={{ color: 'black' }}>Country</InputLabel>
+                        <FormControl fullWidth>
+                            <Select {...register('country')} defaultValue='' onChange={handleCountryChange}>
+                                {countries.map((country) => (
+                                    <MenuItem key={country.isoCode} value={country.name}>
+                                        {country.name}
+                                    </MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
                     </Box>
                     <Box sx={{ mt: 2 }}>
-                        <InputLabel id="demo-simple-select-label" required sx={{ color: 'black' }}>Select State</InputLabel>
-                        <Controller
-                            name="state"
-                            control={control}
-                            rules={{ required: true }}
-                            render={({ field }) => (
-                                <Select
-                                    {...field}
-                                    fullWidth
-                                    inputProps={{
-                                        name: 'state',
-                                        id: 'state',
-                                    }}
-                                >
-                                    {selectedStates && selectedStates.map(state => (
-                                        <MenuItem key={state.stateCode} value={state.name}>
-                                            {state.name}
-                                        </MenuItem>
-                                    ))}
-                                </Select>
-                            )}
-                        />
+                        <InputLabel required sx={{ color: 'black' }}>State</InputLabel>
+                        <FormControl fullWidth>
+                            <Select defaultValue='' {...register('state')}>
+                                <MenuItem value=''>Select a state</MenuItem>
+                                {states.map((state) => (
+                                    <MenuItem key={state.isoCode} value={state.name}> {/* Convert to string */}
+                                        {state.name}
+                                    </MenuItem>
+
+                                ))}
+                            </Select>
+                        </FormControl>
                     </Box>
                     <Box sx={{ mt: 2 }}>
                         <InputLabel required sx={{ color: 'black' }}>Speciality</InputLabel>
@@ -136,47 +175,6 @@ const ChefFirstLoginForm = () => {
                         </FormControl>
                     </Box>
 
-                    <Box sx={{ mt: 2 }}>
-                        <InputLabel sx={{ color: 'black' }}>Certificate of Qualification</InputLabel>
-                        <Controller
-                            name="certificate"
-                            control={control}
-                            defaultValue=""
-                            render={({ field }) => (
-                                <Paper
-                                    elevation={0}
-                                    variant='outlined'
-                                    sx={{
-                                        display: 'flex',
-                                        flexDirection: 'column',
-                                        alignItems: 'center',
-                                        padding: '20px',
-                                    }}
-                                >
-                                    <label htmlFor="certificate-upload">
-                                        <input
-                                            id="certificate-upload"
-                                            type="file"
-                                            style={{ display: 'none' }}
-                                            {...field}
-                                            onChange={(e) => {
-                                                field.onChange(e);
-                                                handleFileChange(e);
-                                            }}
-                                        />
-                                        <Box sx={{ textAlign: 'center' }}>
-                                            <CloudUploadIcon fontSize="large" color="primary" />
-                                            <Typography variant="body1" color="textSecondary" mt={1}>
-                                                <span style={{ color: 'blue' }}>Click to upload</span> or drag and drop
-                                                <br />
-                                                PDF, TXT (Max Size 5MB)
-                                            </Typography>
-                                        </Box>
-                                    </label>
-                                </Paper>
-                            )}
-                        />
-                    </Box>
 
 
                     <Box sx={{ mt: 2 }}>
@@ -189,14 +187,14 @@ const ChefFirstLoginForm = () => {
                                     height="200"
                                     image={previewImage}
                                     sx={{
-                                        borderRadius: '50%', 
-                                        width: '60%'
+                                        borderRadius: '50%',
+                                        width: '60%',
                                     }}
                                 />
                             )}
                         </Box>
                         <Controller
-                            name="image"
+                            name="profile_picture"
                             control={control}
                             render={({ field }) => (
                                 <TextField
@@ -214,6 +212,21 @@ const ChefFirstLoginForm = () => {
                         />
                     </Box>
 
+                    {showFileUpload && (
+                        <Box sx={{ mt: 2 }}>
+                            <InputLabel sx={{ color: 'black' }}>Additional Files</InputLabel>
+                            <FileUpload onFileDataChange={setAdditionalFileData} additionalFileData={additionalFileData} />
+                        </Box>
+                    )}
+
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={toggleFileUpload}
+                        sx={{ mt: 2 }}
+                    >
+                        {showFileUpload ? 'Hide File Upload' : 'Show File Upload'}
+                    </Button>
 
                     <Box sx={{ mt: 3, display: 'flex', justifyContent: 'flex-end' }}>
                         <Button variant="contained" color="primary" type="submit">
@@ -222,8 +235,8 @@ const ChefFirstLoginForm = () => {
                     </Box>
                 </Box>
             </Box>
-        </Dialog >
-    )
-}
+        </Dialog>
+    );
+};
 
-export default ChefFirstLoginForm
+export default ChefFirstLoginForm;
---
+++ frontend/src/components/chefs/FileInput.tsx
@@ -0,0 +1,27 @@
+import { Box, TextField,IconButton } from '@mui/material';
+import { Delete } from '@mui/icons-material';
+
+
+const FileInput = ({ index, file, onFileChange, onDescriptionChange, onRemoveFile }) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', gap: '16px', mt: '8px' }}>
+      <TextField
+        label={`File ${index + 1} Description`}
+        fullWidth
+        variant="outlined"
+        value={file.description}
+        onChange={(e) => onDescriptionChange(index, e.target.value)}
+      />
+      <input
+        type="file"
+        accept=".pdf,.doc,.docx,.jpg,.png"
+        onChange={(e) => onFileChange(index, e.target.files[0])}
+      />
+      <IconButton onClick={() => onRemoveFile(index)}>
+        <Delete />
+      </IconButton>
+    </Box>
+  );
+};
+
+export default FileInput;
