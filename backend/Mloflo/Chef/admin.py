3ebd6866bfa19e2885db07dc83b9840ed34af026
FaithSang <faithsang001@gmail.com>
2023-09-07 16:07:22 +0300

    Updated the Chef profile to include files and descriptions

--- backend/Mloflo/Chef/admin.py
+++ backend/Mloflo/Chef/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from . models import Chef
+from . models import Chef,Certification
 
 # Register your models here.
 admin.site.register(Chef)
+admin.site.register(Certification)
