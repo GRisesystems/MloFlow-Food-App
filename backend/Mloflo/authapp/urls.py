14026cc796b022371707a0057931409dbb4cc160
engkamakia <georgekamakia@gmail.com>
2023-09-13 19:04:18 +0300

    feature to regenerate the OTP

--- backend/Mloflo/authapp/urls.py
+++ backend/Mloflo/authapp/urls.py
@@ -4,13 +4,11 @@ from django.urls import path
 from .views import *
 
 
-from . import views
-
 urlpatterns = [  
     
     path('register/', RegisterView.as_view(), name="register"),
     path('verify-otp/', OTPVerificationView.as_view(), name='verify-otp'),
-    path('regenerate-otp/', views.generate_and_send_otp, name='verify-otp'),
-    path("login/", views.LoginView.as_view(), name="login"),
+    path('regenerate-otp/', RegenerateOTPView.as_view(), name='verify-otp'),
+    path("login/", LoginView.as_view(), name="login"),
     
 ]
