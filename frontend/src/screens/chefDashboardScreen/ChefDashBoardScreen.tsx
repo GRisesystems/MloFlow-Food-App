d88963f4af983da84a639077e871ea01a09ed998
wairatimurage <wairatiengineer21@gmail.com>
2023-09-15 12:25:49 +0300

    Rvert changes

--- frontend/src/screens/chefDashboardScreen/ChefDashBoardScreen.tsx
+++ frontend/src/screens/chefDashboardScreen/ChefDashBoardScreen.tsx
@@ -1,114 +1,68 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { Tab, Tabs, Paper, Switch } from '@mui/material';
 import HireRequestsTab from './HireRequestsTab';
 import PendingRequestsTab from './PendingRequestsTab';
 import HistoryTab from './History';
-import Switch from '@mui/material/Switch';
 import { useAuth } from '../../utils/AuthContext';
-import ChefFirstLoginForm from '../../components/chefs/ChefFirstLoginForm';
 
-
-// Define your styled components here
 const ChefDashboardContainer = styled.div`
-  display: flex;
+  display: flex;  
   flex-direction: column;
   align-items: center;
   margin-top: 20px;
   margin-left: 20px;
 `;
 
-// const WelcomeMessage = styled.h1`
-//   font-size: 24px;
-//   margin-bottom: 20px;
-// `;
-
-const TabMenu = styled.div`
-  display: flex;
-  width: 100%;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-const TabButton = styled.button<{ isActive: boolean }>` // <-- Add backtick here
-  flex: 1;
-  padding: 20px;
-  background-color: ${props => (props.isActive ? '#FFA000' : '#f0f0f0')};
-  color: ${props => (props.isActive ? 'black' : 'black')};
-  border: none;
-  border-radius: 0;
-  cursor: pointer;
-`;
-
-const TabContentContainer = styled.div`
-  width: 100%;
-`;
 const AvailabilityContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
 `;
-const ChefDashboard = () => {
-  const [selectedTab, setSelectedTab] = useState<number>(0);
-  const [isAvailable, setIsAvailable] = useState<boolean>(false);
-  const [acceptedRequests, setAcceptedRequests] = useState<number[]>([]); // Maintain state here
-  const [deniedRequests, setDeniedRequests] = useState<number[]>([]); // Maintain state here
-  const [selectedRequest, setSelectedRequest] = useState<number | null>(null); // Specify the type as 'number | null'
 
-  const { accessToken, updateProfileData,isProfileComplete } = useAuth();
-  // console.log('is profile complete:',isProfileComplete)
+const CustomTabs = styled(Tabs)`
+  background-color: #fbb31d; // Background color
+  width: 100%; // Fill entire screen width
+`;
+
+const CustomTab = styled(Tab)`
+  color: black; // Tab text color
+`;
+
+const ChefDashboard = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const [isAvailable, setIsAvailable] = useState(false);
+  useAuth();
 
   const handleAvailabilityChange = () => {
     setIsAvailable(!isAvailable);
   };
-  const handleRequestSelect = (requestId: number) => {
-    setSelectedRequest(requestId);
-  };
-  // Define your tab-specific components here
-  const tabComponents = [
-    <HireRequestsTab
-      acceptedRequests={acceptedRequests}
-      deniedRequests={deniedRequests}
-      setAcceptedRequests={setAcceptedRequests}
-      setDeniedRequests={setDeniedRequests}
-      onSelectRequest={handleRequestSelect}
-  selectedRequest={selectedRequest}
-    />,
-    <PendingRequestsTab
-      acceptedRequests={acceptedRequests}
-      deniedRequests={deniedRequests} selectedRequest={null}/>,
-    <HistoryTab
-      acceptedRequests={acceptedRequests}
-      deniedRequests={deniedRequests}
-    />,
-  ];
 
-  const handleTabChange = (tab: number) => {
-    setSelectedTab(tab);
+  const handleTabChange = (_event: any, newValue: React.SetStateAction<number>) => {
+    setSelectedTab(newValue);
   };
 
   return (
     <ChefDashboardContainer>
-      {/* <WelcomeMessage>Welcome Chef!</WelcomeMessage>
-      {isProfileComplete ? <></>: <ChefFirstLoginForm/>} */}
-      {isProfileComplete ? <></>: <ChefFirstLoginForm/>} 
       <AvailabilityContainer>
         <p>Welcome Chef! Your Availability:</p>
         <Switch checked={isAvailable} onChange={handleAvailabilityChange} />
       </AvailabilityContainer>
-      <TabMenu>
-        {tabComponents.map((tab, index) => (
-          <TabButton
-            key={index}
-            isActive={selectedTab === index}
-            onClick={() => handleTabChange(index)}
-          >
-            {index === 0 ? 'Hire Requests' : index === 1 ? 'Pending Requests' : 'History'}
-          </TabButton>
-        ))}
-      </TabMenu>
-      <TabContentContainer>
-        {tabComponents[selectedTab]}
-      </TabContentContainer>
+      <Paper square elevation={3}>
+        <CustomTabs
+          value={selectedTab}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={handleTabChange}
+        >
+          <CustomTab label="Hire Requests" />
+          <CustomTab label="Pending Requests" />
+          <CustomTab label="History" />
+        </CustomTabs>
+      </Paper>
+      {selectedTab === 0 && <HireRequestsTab />}
+      {selectedTab === 1 && <PendingRequestsTab acceptedRequests={[]} deniedRequests={[]} selectedRequest={null} />}
+      {selectedTab === 2 && <History />}
     </ChefDashboardContainer>
   );
 };
