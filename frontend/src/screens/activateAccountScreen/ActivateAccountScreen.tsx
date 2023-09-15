ef517211d3cf81102d9a4ba1d54c49a5fc99cd3e
isaac <isaac@localhost.localdomain>
2023-09-15 15:21:53 +0300

    password reset and account activation

--- frontend/src/screens/activateAccountScreen/ActivateAccountScreen.tsx
+++ frontend/src/screens/activateAccountScreen/ActivateAccountScreen.tsx
@@ -1,21 +1,71 @@
-import { Box, Button } from '@mui/material';
-import {useState} from 'react'
-import { useSearchParams } from 'react-router-dom';
+import { useState } from 'react';
+import OtpInput from 'react-otp-input';
+import { Box, Button, Typography } from '@mui/material';
 
 const ActivateAccountScreen = () => {
-  const[verified,setVerified] = useState(false)
-  const [uuid,token] = useSearchParams()
+  const [otp, setOtp] = useState('');
 
-  const verify_account =()=>{
+  const verifyAccount = () => {
+    console.log(otp)
+  };
 
-  }
+  const clearOtp = () => {
+    setOtp('');
+  };
+
   return (
-    <Box>
-      <Button variant='outlined' onClick={()=>console.log(uuid)}>
-        Verify Account
-      </Button>
+    <Box
+      sx={{
+        minHeight: '80vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        mt: 1,
+        mx: 'auto',
+        width: '80%',
+        boxShadow: 2
+      }}
+    >
+      <Box sx={{ p: 2 }}>
+        <Typography variant='h6' sx={{ textAlign: 'center', mb: 3, fontWeight: 'bold' }}>Enter Account activation number</Typography>
+        <OtpInput
+          value={otp}
+          onChange={setOtp}
+          numInputs={6}
+          renderSeparator={<span>-</span>}
+          renderInput={(props) => <input {...props} />}
+          inputStyle={{
+            width: '50px',
+            height: '50px',
+            fontSize: '24px',
+            margin: '4px',
+            borderRadius: '8px',
+            border: '1px solid black',
+          }}
+        />
       </Box>
-  )
-}
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <Button
+          variant="outlined"
+          onClick={clearOtp}
+          disabled={otp === ''}
+          sx={{ marginTop: '16px' }}
+        >
+          Clear
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={verifyAccount}
+          disabled={otp.length !== 6}
+          sx={{ marginTop: '16px', ml: 2 }}
+        >
+          Activate
+        </Button>
+      </Box>
+    </Box>
+  );
+};
 
-export default ActivateAccountScreen
+export default ActivateAccountScreen;
---
+++ frontend/src/screens/forgotPasswordScreen/ForgotPasswordScreen.jsx
@@ -0,0 +1,12 @@
+import Box from '@mui/material/Box'
+import ForgotPasswordForm from '../../components/forgotPassword/ForgotPasswordForm'
+
+const ForgotPasswordScreen = () => {
+  return (
+    <Box>
+      <ForgotPasswordForm />
+    </Box>
+  )
+}
+
+export default ForgotPasswordScreen
