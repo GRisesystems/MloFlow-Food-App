c285f9b1933badc0e66bf6de4ac6a894db5f6ef2
isaac <isaac@localhost.localdomain>
2023-09-29 12:02:07 +0300

    admin section total customer count

--- frontend/src/screens/adminScreens/AdminCustomerScreen.jsx
+++ frontend/src/screens/adminScreens/AdminCustomerScreen.jsx
@@ -5,11 +5,12 @@ import CustomerTable from '../../components/adminComponents/customer/CustomerTab
 import customer_data from './testData/customer_data.json'
 
 const AdminCustomerScreen = () => {
+  const customer_count = customer_data.length
   return (
     <Box>
       <Grid container spacing={2}>
         <Grid item md={4} lg={3} sm={12} xs={12}>
-          <CustomerTopCard amount={10} />
+          <CustomerTopCard amount={customer_count} />
         </Grid>
       </Grid>
       <CustomerTable customer_data={customer_data} />
